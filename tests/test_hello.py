543d172d43d0a0309557655e6f6d359989adcde9
lingdai0012 <ling_dai@outlook.com>
2024-01-12 23:50:35 +0100

    add test structure

---
+++ tests/test_hello.py
@@ -0,0 +1,11 @@
+import unittest
+from app.hello import hello_word
+
+
+class TestHelloWord(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual("hello world", hello_word())
+
+if __name__ == "__main__":
+    unittest.main()
+
