3de78d263f299eb3fa23baf368f281472720050c
lingdai0012 <ling_dai@outlook.com>
2024-01-13 21:21:16 +0100

    create with_session decorator for re-using session

--- app/orm/database.py
+++ app/orm/database.py
@@ -1,3 +1,13 @@
 import wrapt
+from app import DATABASE_URL
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+__engine = create_engine(DATABASE_URL)
+SESSION = sessionmaker(bind=__engine, autoflush=False, autocommit=False)
+
+
+@wrapt.decorator
+def with_session(wrapped, instance, args, kwargs):
+    with SESSION.begin() as session:
+        return wrapped(*args, **kwargs, session=session)
