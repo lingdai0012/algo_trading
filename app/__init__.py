536728c1f29ab54560a819d01c2ff94aa2fc9cea
lingdai0012 <ling_dai@outlook.com>
2024-01-13 11:15:19 +0100

    add config

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,12 @@ import os
 load_dotenv()
 API_KEY = os.environ["API_KEY"]
 API_SECRETE = os.environ["API_SECRETE"]
+DB_HOST = os.environ["DB_HOST"]
+DB_USERNAME = os.environ["DB_USERNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
